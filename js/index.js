7e8241907fb550e81d6c5b5644b27161c6f99eef
sena <53222099+sena-himeno@users.noreply.github.com>
2023-04-10 19:17:15 +0800

    7/1826

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const outputSoundEl = document.getElementById("key_output");
 const luncher = document.getElementById("game_start");
 const key_print = document.getElementById("key_print");
+const song_img = document.getElementById("song_img");
+const prev_song = document.getElementById("prev_song");
+const next_song = document.getElementById("next_song");
 
 
 var success_count;
 var next_count;
+
+var txt_path;
 
 var sound_name;
 var count = 0;
@@ -12,12 +17,48 @@ var print_content;
 var audio_player = new Audio();
 var musicEl = audio_player;
 
+// const txt_path = "./SR01/key.txt";
+// audio_player.src = "/page/SR/SR01/song.ogg"
+
+
 const song_info = {
-
+    1:{
+        "song_title_img_path":"/page/SR/SR01/img/songtitle.jpg",
+        "song_number":"SR01",
+        "song_name":" unknown ",
+        "song_key_info_path":"/page/SR/SR01/key.txt",
+        "song_key_sound_path":"/page/SR/SR01/Key",
+        "song_path":"/page/SR/SR01/song.ogg",
+        "song_img_path": "/page/SR/SR01/img"
+    },
+    2:{
+        "song_title_img_path":"/page/SR/SR01/img/songtitle.jpg",
+        "song_number":"SR01",
+        "song_name":" unknown 2 ",
+        "song_key_info_path":"/page/SR/SR01/key.txt",
+        "song_key_sound_path":"/page/SR/SR01/Key",
+        "song_path":"",
+        "song_img_path": "/page/SR/SR01/img"
+    },
+    3:{
+        "song_title_img_path":"/page/SR/SR01/img/songtitle.jpg",
+        "song_number":"SR01",
+        "song_name":" unknown 3 ",
+        "song_key_info_path":"/page/SR/SR01/key.txt",
+        "song_key_sound_path":"/page/SR/SR01/Key",
+        "song_path":"/page/SR/SR01/song.ogg",
+        "song_img_path": "/page/SR/SR01/img"
+    },
+    4:{
+        "song_title_img_path":"/page/SR/SR01/img/songtitle.jpg",
+        "song_number":"SR01",
+        "song_name":" unknown 3 ",
+        "song_key_info_path":"/page/SR/SR01/key.txt",
+        "song_key_sound_path":"/page/SR/SR01/Key",
+        "song_path":"/page/SR/SR01/song.ogg",
+        "song_img_path": "/page/SR/SR01/img"
+    }
 }
-
-const txt_path = "./SR01/key.txt";
-audio_player.src = "/page/SR/SR01/song.ogg"
 
 
 class Play {
@@ -34,30 +75,35 @@ class Play {
         count = 0;
         this.print_content = "";
         sound_name = this.key_info[0].sound_name;
+
+
     }
     run() {
         this.init();
         audio_player.play();
         this.play_main();
         luncher.style.display = "none";
+        select_Song.style.display="none";
+        img_box.style.display="block";
     }
     end() {
         luncher.style.display = "block";
+        select_Song.style.display="block";
+        img_box.style.display="none";
+
     }
     async play_main() {
         let interval = setInterval(() => {
-
-            if (count == this.key_info.length) {
-                clearInterval(interval)
+            if (musicEl.paused) {
                 this.end()
+                clearInterval(interval)
             }
-
             // print key
-            this.print_key();
+            if(count < this.key_info.length){
+                this.print_key();
+            }
             console.log(this.print_content)
             clog(count)
-
-
         }, 100)
     }
     print_key() {
@@ -113,102 +159,132 @@ class Key_Sound {
         }, 100)
     }
     key_sound() {
+        outputSoundEl.load()
         outputSoundEl.setAttribute("src", "./SR01/Key/"+this.sound_name+".ogg")
         outputSoundEl.play();
     }
     sync_key_sound() {
         this.currentTime = musicEl.currentTime
         this.keyPressedTime = Math.floor(this.currentTime * 10) / 10
-        clog(this.keyPressedTime  + " and " + this.key_info[this.next_count].keytime)
-        if (this.keyPressedTime == parseFloat(this.key_info[this.next_count].keytime)) {
-            this.sound_name = this.key_info[this.next_count].sound_name;
-            this.next_count++;
-            clog("sync_key_sound,cur_key_sound_name " + sound_name);
-            outputSoundEl.setAttribute("src", "./SR01/Key/"+this.sound_name+".ogg")
-            outputSoundEl.play();
-
+        if(count < this.key_info.length){
+            if (this.keyPressedTime == parseFloat(this.key_info[this.next_count].keytime)) {
+                this.sound_name = this.key_info[this.next_count].sound_name;
+                this.next_count++;
+                clog("sync_key_sound,cur_key_sound_name " + sound_name);
+                this.key_sound()
+                
+            }
         }
         clog("Next key: " + this.key_info[this.next_count].keyPressed + ", Time: " + this.key_info[this.next_count].keytime);
     }
 
 }
-class Select_Soung{
-    constructor(){
 
+class Select_Soung{
+    constructor(song_info){
+        this.song_info = song_info;
+        this.cur_indext
+        this.song_info_length
+        this.txt_path
+        this.song_path
     }
     init(){
+        this.cur_index = 1;
+        this.song_info_length = this.get_song_info_length()
+        prev_song.style.display="none";
+        this.txt_path = this.song_info[this.cur_index].song_key_info_path;
+        this.song_path = this.song_info[this.cur_index].song_path;
+    }
+    get_song_info_length(){
+        var length = 0;
+        for(var i in this.song_info){
+            length++
+        }
+        return length;
+    }
+    change_song_info(){
+        // clog(txt_path)
+        clog( "cur_index " + this.cur_index + " and length " + this.song_info_length)
+        console.log(this.cur_index + " " + this.song_info[this.cur_index].song_name)
+        this.txt_path  = this.song_info[this.cur_index].song_key_info_path;
+        song_img.src = this.song_info[this.cur_index].song_title_img_path;
+        this.song_path = this.song_info[this.cur_index].song_path;
+    }
+    rule(){
+        if(this.cur_index <= 1){
+            prev_song.style.display="none";
+        }else{
+            prev_song.style.display="block";
+        }
+        if(this.cur_index >= this.song_info_length){
+            
+            next_song.style.display="none";
+        }else{
+            next_song.style.display="block";
+        }
+    }
+    async run(){
+        prev_song.onclick = () => {
+            this.cur_index--;
+            this.rule();
+            this.change_song_info();
+        }
+        next_song.onclick = () => {
+            this.cur_index++;
+            this.rule();
+            this.change_song_info();
 
+        }
+        this.change_song_info();
     }
-    run(){
-        
+    get_txt_path(){
+        clog(this.txt_path)
+        return this.txt_path;
     }
+    get_song_path(){
+        clog(this.song_path)
+        return this.song_path;
+    }
+
+    
 }
 
 async function main() {
+    
+    var song = new Select_Soung(song_info);
+    song.init();
+    song.run();
 
-    let key_info = await txt_to_json(txt_path)
-    let gameContext = {
-        "sheetData": key_info,
-        "count": 0,
-        "keyPressed": ""
-    }
-    luncher.onclick = () => {
-        console.log("1")
-        var temp = new Play(key_info, musicEl, audio_player);
-        temp.run();
+    luncher.onclick = async() => {
+        txt_path = song.get_txt_path();
+        audio_player.src = song.get_song_path();
+        let key_info = await txt_to_json(txt_path)
+        var player = new Play(key_info, musicEl, audio_player);
+        player.run();
         var key_sound_controler = new Key_Sound(key_info, outputSoundEl, audio_player, musicEl)
         key_sound_controler.init();
         key_sound_controler.run();
-    }
-    draw_line(ctx, 30, 10);
-
-    // document.onkeyup = function (event) {
-    //     if (!musicEl.paused) {
-    //         gameContext.keyPressed = event.key;
-
-    //         sound_controller(gameContext);
-    //     }
-    // }
+        
+    }   
+    window.addEventListener("load",function(){
+        draw_line(ctx, 30, 10);
+    })
 }
-
-function sound_controller(gameContext) {
-    const { sheetData, _count, keyPressed } = gameContext;
-    const currentTime = musicEl.currentTime
-    let keyPressedTime = Math.floor(currentTime * 10) / 10
-    clog(`${keyPressedTime}  ${keyPressed}`)
-
-    if (currentTime >= parseFloat(sheetData[gameContext.count].keytime)) {
-        sound_name = sheetData[gameContext.count].sound_name;
-        gameContext.count++;
-    } else {
-        clog("You missed the key.");
-    }
-    outputSoundEl.setAttribute("src", `./SR01/Key/${sound_name}.ogg`)
-    outputSoundEl.play();
-    // clog(sound_name)
-    clog(`Next key: ${sheetData[gameContext.count].keyPressed}, Time: ${sheetData[gameContext.count].keytime}`)
-}
-
 
 async function txt_to_json(txt_path) {
-
     let result = [];
     let text = await fetch(txt_path)
         .then(response => response.text())
-
     let lineCount = 0;
     text.split("\n").forEach(line => {
         let fields = [];
         let inlineIndex = 0;
-
-
         line.split(",").forEach(info => {
             if (inlineIndex <= 3) {
                 fields[inlineIndex] = info;
             }
             inlineIndex++;
         })
-
         // let accurate_key = {};
         let key_time = String(Math.floor(fields[1] * 10) / 10)
 
